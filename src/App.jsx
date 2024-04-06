3bd6c92e677d4891a772dd0b7712b1a764766edc
unknown <gagas112212@gmail.com>
2024-04-06 15:29:18 +0700

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import "./index.css";
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
 
 const App = () => {
@@ -16,7 +17,11 @@ const App = () => {
         <Benefits />
         <Collaboration />
         <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
+
       <ButtonGradient />
     </>
   );
